347d3e278f74b9da9bac6dbf02de4a43e18cb7c9
imdepak <deepak.v@outlook.com>
2025-01-30 17:37:44 +0530

    Update Jenkin file

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ const App = () => {
 
   return (
     <div>
-      <p>Hi There, I m Running  from CI/CD Testing...</p>
+      <p>Hi There, I m Running for JayaGeda...</p>
       {!isOnline && (
         <div className="notification">
           You are currently offline. Please check your internet connection.
